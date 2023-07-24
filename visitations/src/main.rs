0acf20b44db34ea50e54c5a31a186c7e3712c972
Teimurazi <toloraia@mit.edu>
2023-07-24 18:10:42 +0400

    modified main.rs

--- visitations/src/main.rs
+++ visitations/src/main.rs
@@ -5,16 +5,10 @@ use text_io::read;
 use typedb_client::{
     Connection, DatabaseManager, Options, Session,
     SessionType::{Data, Schema},
-    TransactionType::{Read, Write},
+    TransactionType::{Read, Write}, // transaction, Transaction,
 };
 
-#[derive(Debug)]
-enum HandleError {
-    // Io(io::Error),
-    TypeDB(typedb_client::error::Error),
-}
-
-const DATABASE: &str = "database describing tourists visiti";
+const DATABASE: &str = "database describing tourists visiting cities from countries";
 
 fn new_core_connection() -> typedb_client::Result<Connection> {
     Connection::new_plaintext("localhost:1729")
@@ -36,39 +30,33 @@ fn create_person_entity(firstname: &str, lastname: &str) -> String {
     format!("insert $x isa person, has firstname \"{firstname}\", has lastname \"{lastname}\";")
 }
 
-async fn create_schema(connection: Connection) -> Result<(),  HandleError> {
+async fn create_schema(connection: Connection) -> typedb_client::Result {
     let schema = get_schema();
     let databases = DatabaseManager::new(connection.clone());
     if databases
         .contains(DATABASE)
-        .await
-        .map_err(HandleError::TypeDB)?
+        .await?
     {
         println!("\nSchema Already Defined\n");
     } else {
         databases
             .create(DATABASE)
-            .await
-            .map_err(HandleError::TypeDB)?;
+            .await?;
         let session = Session::new(
             databases
                 .get(DATABASE)
-                .await
-                .map_err(HandleError::TypeDB)?,
+                .await?,
             Schema,
         )
-        .await
-        .map_err(HandleError::TypeDB)?;
+        .await?;
         let transaction = session
             .transaction(Write)
-            .await
-            .map_err(HandleError::TypeDB)?;
+            .await?;
         transaction
             .query()
             .define(schema.as_str())
-            .await
-            .map_err(HandleError::TypeDB)?;
-        transaction.commit().await.map_err(HandleError::TypeDB)?;
+            .await?;
+        transaction.commit().await?;
         drop(session);
         println!("\nSchema Defined Successfully\n");
     }
@@ -91,7 +79,7 @@ fn create_visited_relationship(
     )
 }
 
-async fn create_data(connection: Connection) ->  Result<(), HandleError> {
+async fn create_data(connection: Connection) ->  typedb_client::Result  {
     let databases = DatabaseManager::new(connection.clone());
     // insert data
     let session = Session::new(databases.get(DATABASE).await.unwrap(), Data)
@@ -101,23 +89,19 @@ async fn create_data(connection: Connection) ->  Result<(), HandleError> {
     let cities = ["Tbilisi", "London", "Cambridge", "Zugdidi"];
     cities
         .iter()
-        .map(|city| {
-            let _ = transaction.query().insert(&create_city_entity(city));
-        })
+        .map(|city| transaction.query().insert(&create_city_entity(city)))
         .count();
     let countries = ["Georgia", "United Kingdom", "United States of America"];
     countries
         .iter()
-        .map(|country| {
-            let _ = transaction.query().insert(&create_country_entity(&country));
-        })
+        .map(|country| transaction.query().insert(&create_country_entity(&country)))
         .count();
     // Georgians
 
-    // let geo_first_names = ["Teimurazi", "Natalia", "Luka", "Irakli", "Giorgi", "Robiko"];
-    // let geo_last_names = ["Toloraia", "Skhulukhia", "Tsintsadze"];
-    let geo_first_names = ["Teimurazi", "Natalia"];
-    let geo_last_names = ["Toloraia", "Skhulukhia"];
+    let geo_first_names = ["Teimurazi", "Natalia", "Luka", "Irakli", "Giorgi", "Robiko"];
+    let geo_last_names = ["Toloraia", "Skhulukhia", "Tsintsadze"];
+    // let geo_first_names = ["Teimurazi", "Natalia"];
+    // let geo_last_names = ["Toloraia", "Skhulukhia"];
     let mut duration = 0;
     let mut start = 9;
     for first_name in geo_first_names {
@@ -135,11 +119,6 @@ async fn create_data(connection: Connection) ->  Result<(), HandleError> {
                 &create_visited_relationship(start, end, first_name, last_name, "Cambridge", 1)
                     .as_str(),
             );
-
-            let _ = transaction.query().insert(
-                &create_visited_relationship(start, end, first_name, last_name, "London", 3)
-                    .as_str(),
-            );
         }
     }
 
@@ -147,10 +126,10 @@ async fn create_data(connection: Connection) ->  Result<(), HandleError> {
 
     duration = 3;
     start = 9;
-    // let uk_first_names = ["James", "Olivia", "William", "Amelia", "Jack"];
-    // let uk_last_names = ["Davies", "Patel", "Mitchell"];
-    let uk_first_names = ["James", "Olivia"];
-    let uk_last_names = ["Davies", "Patel"];
+    let uk_first_names = ["James", "Olivia", "William", "Amelia", "Jack"];
+    let uk_last_names = ["Davies", "Patel", "Mitchell"];
+    // let uk_first_names = ["James", "Olivia"];
+    // let uk_last_names = ["Davies", "Patel"];
     for first_name in uk_first_names {
         start += 1;
         for last_name in uk_last_names {
@@ -166,11 +145,6 @@ async fn create_data(connection: Connection) ->  Result<(), HandleError> {
                 &create_visited_relationship(start, end, first_name, last_name, "Tbilisi", 1)
                     .as_str(),
             );
-
-            let _ = transaction.query().insert(
-                &create_visited_relationship(start, end, first_name, last_name, "Zugdidi", 3)
-                    .as_str(),
-            );
         }
     }
 
@@ -178,10 +152,10 @@ async fn create_data(connection: Connection) ->  Result<(), HandleError> {
 
     duration = 2;
     start = 9;
-    // let us_first_names = ["John", "Emma", "Michael", "Sophia", "David"];
-    // let us_last_names = ["Smith", "Johnson", "Williams"];
-    let us_first_names = ["John", "Emma"];
-    let us_last_names = ["Smith", "Johnson"];
+    let us_first_names = ["John", "Emma", "Michael", "Sophia", "David"];
+    let us_last_names = ["Smith", "Johnson", "Williams"];
+    // let us_first_names = ["John", "Emma"];
+    // let us_last_names = ["Smith", "Johnson"];
     for first_name in us_first_names {
         start += 1;
         for last_name in us_last_names {
@@ -197,11 +171,6 @@ async fn create_data(connection: Connection) ->  Result<(), HandleError> {
                 &create_visited_relationship(start, end, first_name, last_name, "Tbilisi", 1)
                     .as_str(),
             );
-
-            let _ = transaction.query().insert(
-                &create_visited_relationship(start, end, first_name, last_name, "Zugdidi", 3)
-                    .as_str(),
-            );
         }
     }
 
@@ -215,21 +184,21 @@ fn get_the_list_of_cities(_connection: Connection) -> Vec<&'static str> {
     vec!["London", "Tbilisi", "Zugdidi", "Cambridge"]
 }
 
-async fn get_most_visited_city(connection: Connection) -> String {
+async fn get_most_visited_city(connection: Connection) -> typedb_client::Result<String> {
     let cities = get_the_list_of_cities(connection.clone());
     let mut best_city = "";
     let mut answer = -1;
     for city in cities {
-        let num = number_of_times_visited(connection.clone(), city).await;
+        let num = number_of_times_visited(connection.clone(), city).await?;
         if num > answer {
             best_city = city;
             answer = num;
         }
     }
-    best_city.to_string()
+    Ok(best_city.to_string())
 }
 
-async fn number_of_times_visited(connection: Connection, city: &str) -> i64 {
+async fn number_of_times_visited(connection: Connection, city: &str) -> typedb_client::Result<i64> {
     let databases = DatabaseManager::new(connection.clone());
     let session = Session::new(databases.get(DATABASE).await.unwrap(), Data)
         .await
@@ -252,7 +221,7 @@ async fn number_of_times_visited(connection: Connection, city: &str) -> i64 {
         .match_aggregate(&query.as_str())
         .await
         .unwrap();
-    answer.into_i64()
+    Ok(answer.into_i64())
 }
 
 async fn friends_met(connection: Connection, firstname: &str, lastname: &str) -> i64 {
@@ -286,7 +255,7 @@ async fn friends_met(connection: Connection, firstname: &str, lastname: &str) ->
 }
 
 #[tokio::main]
-async fn main() ->  Result<(), HandleError> {
+async fn main() ->  typedb_client::Result  {
     let connection = new_core_connection().expect("creating connection");
 
     create_schema(connection.clone()).await?;
@@ -307,12 +276,12 @@ async fn main() ->  Result<(), HandleError> {
                 let city: String = read!();
                 println!(
                     "{} people visited city {city}",
-                    number_of_times_visited(connection.clone(), city.as_str()).await
+                    number_of_times_visited(connection.clone(), city.as_str()).await?
                 );
             }
             2 => println!(
                 "most visited city is {}",
-                get_most_visited_city(connection.clone()).await.as_str()
+                get_most_visited_city(connection.clone()).await?.as_str()
             ),
             3 => {
                 println!("Enter the first name of the person");
